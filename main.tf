4ea9aea33e5a667de27bea7b43c1a68eb923e286
Jakub Sokołowski <jakub@status.im>
2019-07-28 19:19:12 -0400

    fixup! port to Terraform 0.12
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -119,8 +119,8 @@ resource "google_compute_instance" "host" {
 resource "cloudflare_record" "host" {
   domain = var.domain
   count  = var.host_count
-  name   = google_compute_instance.host[count.index].metadata.hostname,
-  value  = google_compute_instance.host[count.index].network_interface.0.access_config.0.nat_ip,
+  name   = google_compute_instance.host[count.index].metadata.hostname
+  value  = google_compute_instance.host[count.index].network_interface.0.access_config.0.nat_ip
   type   = "A"
   ttl    = 3600
 }
