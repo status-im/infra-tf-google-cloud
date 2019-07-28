2f278505d8cf297c2b672286590255bbf3c2163a
Jakub Sokołowski <jakub@status.im>
2019-07-28 19:16:25 -0400

    port to Terraform 0.12
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -1,142 +1,145 @@
 /* DERIVED --------------------------------------*/
-locals {
-  stage      = "${terraform.workspace}"
-  dc         = "${var.provider}-${var.zone}"
-  /* always add SSH, Tinc, Netdata, and Consul to allowed ports */
-  open_ports = [
-    "22", "655", "8000", "8301",
-    "${var.open_ports}"
-  ]
-}
-/* RESOURCES ------------------------------------*/
 
-locals = {
+locals {
+  stage = terraform.workspace
+  dc    = "${var.provider}-${var.zone}"
+  /* always add SSH, Tinc, Netdata, and Consul to allowed ports */
+  open_ports = ["22", "655", "8000", "8301", var.open_ports]
+
   tags = [
-    "${var.name}", "${local.stage}", "${var.env}",
-    /* for precise targeting with firewall rules */
+    var.name, local.stage, var.env,
     "${var.name}-${var.env}-${local.stage}",
   ]
-  tags_sorted = "${sort(distinct(local.tags))}"
+  tags_sorted = sort(distinct(local.tags))
 }
 
+/* RESOURCES ------------------------------------*/
+
 resource "google_compute_address" "host" {
-  name       = "${var.name}-${format("%02d", count.index+1)}-${local.dc}-${var.env}-${local.stage}"
-  count      = "${var.count}"
-  lifecycle  = { prevent_destroy = true }
+  name  = "${var.name}-${format("%02d", count.index + 1)}-${local.dc}-${var.env}-${local.stage}"
+  count = var.count
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "google_compute_firewall" "host" {
-  name    = "allow-${var.name}-${var.zone}-${var.env}-${local.stage}"
-  network = "default"
+  name        = "allow-${var.name}-${var.zone}-${var.env}-${local.stage}"
+  network     = "default"
   target_tags = ["${var.name}-${var.env}-${local.stage}"]
 
   allow {
     protocol = "tcp"
-    ports    = ["${local.open_ports}"]
+    ports = local.open_ports
   }
   allow {
     protocol = "udp"
-    ports    = ["${local.open_ports}"]
+    ports = local.open_ports
   }
 }
 
+/* optional DDoS mitigation by blocking IP ranges */
 resource "google_compute_firewall" "deny" {
-  name        = "deny-${var.name}-${var.zone}-${var.env}-${local.stage}"
-  network     = "default"
-  target_tags = ["${var.name}-${var.env}-${local.stage}"]
-  count       = "${length(var.blocked_ips) > 0 ? 1 : 0}"
+  name          = "deny-${var.name}-${var.zone}-${var.env}-${local.stage}"
+  network       = "default"
+  target_tags   = ["${var.name}-${var.env}-${local.stage}"]
+  source_ranges = var.blocked_ips
 
-  /* optional DDoS mitigation by blocking IP ranges */
+  /* Optional */
+  count = length(var.blocked_ips) > 0 ? 1 : 0
+
   deny {
-    protocol      = "tcp"
-    source_ranges = ["${var.blocked_ips}"]
+    protocol = "tcp"
   }
   deny {
-    protocol      = "udp"
-    source_ranges = ["${var.blocked_ips}"]
+    protocol = "udp"
   }
 }
 
 resource "google_compute_instance" "host" {
-  name  = "${var.name}-${format("%02d", count.index+1)}-${local.dc}-${var.env}-${local.stage}"
-  zone  = "${var.zone}"
-  count = "${var.count}"
+  name  = "${var.name}-${format("%02d", count.index + 1)}-${local.dc}-${var.env}-${local.stage}"
+  zone  = var.zone
+  count = var.count
 
-  machine_type = "${var.type}"
+  machine_type = var.type
+
   /* enable changing machine_type */
   allow_stopping_for_update = true
 
-  tags = ["${local.tags_sorted}"]
+  tags = local.tags_sorted
 
   boot_disk {
     initialize_params {
-      image = "${var.image}"
-      size  = "${var.vol_size}"
+      image = var.image
+      size  = var.vol_size
     }
   }
 
   /* Ignore changes to size of boot_disk */
-  lifecycle = {
-    ignore_changes = ["size"]
+  lifecycle {
+    ignore_changes = [size]
   }
 
   network_interface {
     network = "default"
     access_config {
-      nat_ip = "${element(google_compute_address.host.*.address, count.index)}"
+      nat_ip = google_compute_address.host[count.index].address
     }
   }
 
-  metadata {
-    node     = "${var.name}"
-    env      = "${var.env}"
-    group    = "${var.group}"
+  metadata = {
+    node  = var.name
+    env   = var.env
+    group = var.group
     /* This is a hack because we can't use dots in actual instance name */
-    hostname = "${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
+    hostname = "${var.name}-${format("%02d", count.index + 1)}.${local.dc}.${var.env}.${local.stage}"
     /* Enable SSH access */
-    sshKeys  = "${var.ssh_user}:${file(var.ssh_key)}"
+    sshKeys = "${var.ssh_user}:${file(var.ssh_key)}"
   }
 
   /* bootstrap access to host and basic resources */
   provisioner "ansible" {
     plays {
-      playbook = {
+      playbook {
         file_path = "${path.cwd}/ansible/bootstrap.yml"
       }
-      groups = ["${var.group}"]
+      groups = [var.group]
       extra_vars = {
-        hostname         = "${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
-        ansible_host     = "${element(google_compute_address.host.*.address, count.index)}"
-        ansible_ssh_user = "${var.ssh_user}"
-        data_center      = "${local.dc}"
-        stage            = "${local.stage}"
-        env              = "${var.env}"
+        hostname         = "${var.name}-${format("%02d", count.index + 1)}.${local.dc}.${var.env}.${local.stage}"
+        ansible_host     = google_compute_address.host[count.index].address
+        ansible_ssh_user = var.ssh_user
+        data_center      = local.dc
+        stage            = local.stage
+        env              = var.env
       }
     }
   }
 }
 
 resource "cloudflare_record" "host" {
-  domain = "${var.domain}"
-  count  = "${var.count}"
-  name   = "${element(google_compute_instance.host.*.metadata.hostname, count.index)}"
-  value  = "${element(google_compute_instance.host.*.network_interface.0.access_config.0.nat_ip , count.index)}"
+  domain = var.domain
+  count  = var.count
+  name   = google_compute_instance.host[count.index].metadata.hostname,
+  value  = google_compute_instance.host[count.index].network_interface.0.access_config.0.nat_ip,
   type   = "A"
   ttl    = 3600
 }
 
 resource "ansible_host" "host" {
-  inventory_hostname = "${element(google_compute_instance.host.*.metadata.hostname, count.index)}"
-  groups = ["${var.group}", "${local.dc}"]
-  count = "${var.count}"
-  vars {
-    ansible_user   = "admin"
-    ansible_host   = "${element(google_compute_instance.host.*.network_interface.0.access_config.0.nat_ip , count.index)}"
-    hostname       = "${element(google_compute_instance.host.*.metadata.hostname, count.index)}"
-    region         = "${element(google_compute_instance.host.*.zone, count.index)}"
-    dns_entry      = "${element(google_compute_instance.host.*.metadata.hostname, count.index)}.${var.domain}"
-    data_center    = "${local.dc}"
-    stage          = "${local.stage}"
-    env            = "${var.env}"
+  inventory_hostname = google_compute_instance.host[count.index].metadata.hostname,
+
+  groups = [var.group, local.dc]
+  count  = var.count
+
+  vars = {
+    ansible_user = "admin"
+    ansible_host = google_compute_instance.host[count.index].network_interface.0.access_config.0.nat_ip
+    hostname     = google_compute_instance.host[count.index].metadata.hostname
+    region       = google_compute_instance.host[count.index].zone
+    dns_entry    = "${google_compute_instance.host[count.index].metadata.hostname}.${var.domain}"
+    data_center  = local.dc
+    stage        = local.stage
+    env          = var.env
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,17 @@
-locals = {
-  public_ips = "${google_compute_instance.host.*.network_interface.0.access_config.0.nat_ip }"
-  hostnames  = "${google_compute_instance.host.*.metadata.hostname}"
+locals {
+  public_ips = google_compute_instance.host.*.network_interface.0.access_config.0.nat_ip
+  hostnames  = google_compute_instance.host.*.metadata.hostname
 }
 
 output "public_ips" {
-  value = ["${local.public_ips}"]
+  value = [local.public_ips]
 }
 
 output "hostnames" {
-  value = ["${local.hostnames}"]
+  value = [local.hostnames]
 }
 
 output "hosts" {
-  value = "${zipmap(local.hostnames, local.public_ips)}"
+  value = zipmap(local.hostnames, local.public_ips)
 }
+
