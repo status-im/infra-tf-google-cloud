b19e39e0feb3f2c6cbf7f0ff153acc9d4ade7d53
Jakub Sokołowski <jakub@status.im>
2020-09-30 09:41:42 +0200

    add missing types for variables
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 /* DNS ------------------------------------------*/
 
+/* We default to: statusim.net */
 variable "cf_zone_id" {
   description = "ID of CloudFlare zone for host record."
-  /* We default to: statusim.net */
   default     = "14660d10344c9898521c4ba49789f563"
 }
 
@@ -10,85 +10,95 @@ variable "cf_zone_id" {
 
 variable "host_count" {
   description = "Number of hosts to run."
+  type        = number
 }
 
+/* https://cloud.google.com/compute/docs/machine-types */
 variable "type" {
   description = "Type of machine to deploy."
-
-  /* https://cloud.google.com/compute/docs/machine-types */
+  type        = string
   default = "n1-standard-1"
 }
 
 variable "root_vol_size" {
   description = "Size of the base image."
-
-  /* 0 should default to size of base image */
-  default = 10
+  type        = number
+  default     = 10 /* 0 should default to size of base image */
 }
 
+/* Use: gcloud compute disk-types list */
 variable "data_vol_type" {
   description = "Type of the extra data volume."
-  /* Use: gcloud compute disk-types list */
-  default = "pd-balanced"
+  type        = string
+  default     = "pd-balanced"
 }
 
 variable "data_vol_size" {
   description = "Size of the extra data volume."
-  default = 0
+  type        = number
+  default     = 0
 }
 
+/* https://cloud.google.com/compute/docs/regions-zones/ */
 variable "zone" {
   description = "Specific zone in which to deploy hosts."
-
-  /* https://cloud.google.com/compute/docs/regions-zones/ */
+  type        = string
   default = "us-central1-a"
 }
 
+/* Use: 'gcloud compute images list --filter=ubuntu' */
 variable "image" {
   description = "OS image to use when deploying hosts."
-
-  /* Use: 'gcloud compute images list --filter=ubuntu' */
-  default = "ubuntu-os-cloud/ubuntu-2004-lts"
+  type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
 }
 
 variable "provider_name" {
   description = "Short name of the provider used."
-
-  /* Google Cloud */
-  default = "gc"
+  type        = string
+  default     = "gc" /* Google Cloud */
 }
 
 /* CONFIG ----------------------------------------*/
 
 variable "name" {
   description = "Name for hosts. To be used in the DNS entry."
+  type        = string
   default     = "node"
 }
 
 variable "env" {
   description = "Environment for these hosts, affects DNS entries."
+  type        = string
 }
 
 variable "group" {
   description = "Ansible group to assign hosts to."
+  type        = string
 }
 
 variable "domain" {
   description = "DNS Domain to update"
+  type        = string
+}
+
+variable "windows_password" {
+  description = "Password for the windows user."
+  type        = string
 }
 
 /* MODULE ----------------------------------------*/
 
 variable "ssh_user" {
   description = "User used to log in to instance"
+  type        = string
   default     = "root"
 }
 
 variable "ssh_key" {
   description = "Names of ssh public keys to add to created hosts"
-
-  /* TODO this needs to be dynamic */
-  default = "~/.ssh/status.im/id_rsa.pub"
+  type        = string
+  default     = "~/.ssh/status.im/id_rsa.pub" /* TODO this needs to be dynamic */
 }
 
 /* FIREWALL -------------------------------------------*/
