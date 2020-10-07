2a842280ab4a777e7e19fc70c15b733d290612d3
Jakub Sokołowski <jakub@status.im>
2020-10-07 14:57:06 +0200

    upgrade google provider to 3.42.0
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "= 3.40.0"
+      version = "= 3.42.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
